ffb25df649db4f59c2c95c3de80f7693c00044e6
Yulia Gorshkova <yuliosity@gmail.com>
2025-05-13 21:48:06 +0300

    2.3.1. CVar AST

---
+++ lib/cvar.ml
@@ -0,0 +1,17 @@
+type atom = Int of int | Var of string
+
+type expr =
+  | Atom of atom
+  | Prim0 of Ast.prim0
+  | Prim1 of {op: Ast.prim1; arg: atom}
+  | Prim2 of {op: Ast.prim2; l: atom; r: atom}
+
+type stmt = Assign of {v: string; arg: expr}
+
+type label = string
+
+type block = {label: label; stmts: stmt list; return: expr}
+
+type info = {locals: string list}
+
+type program = {info: info; body: block list}
