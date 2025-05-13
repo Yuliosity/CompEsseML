3278455cbc73021a9406bf23bb76e9cb7e3d63ed
Yulia Gorshkova <yuliosity@gmail.com>
2025-05-13 21:31:22 +0300

    Get rid of mutability in uniquify

--- lib/uniquify.ml
+++ lib/uniquify.ml
@@ -4,27 +4,27 @@ module StrMap = Map.Make(String)
 
 type env = int StrMap.t
 
-let i = ref 0
-
 (* Exercise 2.1 *)
-let rec uniquify_expr env = function
-  | Var x -> Var (StrMap.find x env)
+let rec uniquify_expr env = 
+  let mangle v i = v ^ "_" ^ string_of_int i in
+  function
+  | Var v -> Var (mangle v (StrMap.find v env))
   | Prim1 {op; e} -> Prim1 {op; e = uniquify_expr env e}
   | Prim2 {op; l; r} -> Prim2 {op; l = uniquify_expr env l; r = uniquify_expr env r}
   | Let {v; e; body} ->
-    incr i;
-    let name = v ^ "_" ^ string_of_int !i in
-    let env = StrMap.add v name env in
+    let i = match StrMap.find_opt v env with
+      | None -> 1
+      | Some i -> i + 1
+    in
+    let env = StrMap.add v i env in
     Let {
-      v = name ;
+      v = mangle v i;
       e = uniquify_expr env e;
       body = uniquify_expr env body
     }
   | e -> e
 
-let uniquify {body} =
-  i := 0;
-  {body = uniquify_expr StrMap.empty body}
+let uniquify {body} = {body = uniquify_expr StrMap.empty body}
 
 (* Exercise 2.2 *)
 let%test_unit "" =
