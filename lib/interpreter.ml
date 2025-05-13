eed89953ef0b04f760cf067b5c9afa0c871e2a61
Yulia Gorshkova <yuliosity@gmail.com>
2025-05-13 17:10:05 +0300

    Inline tests for the interpreter

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -2,21 +2,31 @@ module StrMap = Map.Make(String)
 
 type env = int StrMap.t
 
+open Ast
+
 class interp_lint = object (self)
   method interp_expr (env: env) : Ast.expr -> int = function
     | Prim0 Read -> read_int ()
     | Int i -> i
-    | Prim1 {op = Neg; e} -> self#interp_expr env e
+    | Prim1 {op = Neg; e} -> -(self#interp_expr env e)
     | Prim2 {op; l; r} -> 
         let l = self#interp_expr env l in
         let r = self#interp_expr env r in
         (match op with
           | Add -> l + r
-          | Sub -> l + r)
+          | Sub -> l - r)
     | _ -> failwith "Unreachable: interp_exp"
   method interp (p: Ast.program) =
     self#interp_expr StrMap.empty p.body
 end
+
+let%test_unit "LInt" =
+  let interp = new interp_lint in
+  let eval_expr e = interp#interp {body = e} in
+  assert (eval_expr (Prim1 {op = Neg; e = Int 10}) = -10);
+  assert (eval_expr (Prim2 {op = Add; l = Int 32; r = Int 10}) = 42);
+  let test3 = Prim2 {op = Sub; l = Int 52; r = Prim2 {op = Add; l = Int 5; r = Int 5}} in
+  assert (eval_expr test3 = 42)
 
 class interp_lvar = object (self)
   inherit interp_lint as super
@@ -27,3 +37,16 @@ class interp_lvar = object (self)
       self#interp_expr (StrMap.add v res env) body
     | e -> super#interp_expr env e
 end
+
+let%test_unit "LVar" =
+  let interp = new interp_lvar in
+  let eval_expr e = interp#interp {body = e} in
+  assert (eval_expr (Prim2 {op = Add; l = Int 32; r = Int 10}) = 42);
+  let test2 =
+    Let {
+      v = "aa";
+      e = Prim2 {op = Add; l = Int 20; r = Int 1};
+      body = Prim2 {op = Add; l = Var "aa"; r = Var "aa"}
+    }
+  in
+  assert (eval_expr test2 = 42)
